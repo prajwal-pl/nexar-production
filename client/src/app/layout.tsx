0a7d23eef371dc8cfba9fbd6392fc13d4c890ed8
Prajwal-PL <prajwalpl096@gmail.com>
2024-09-17 19:42:41 +0530

    init server

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import DashboardWrapper from "./DashboardWrapper";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <DashboardWrapper>{children}</DashboardWrapper>
+        <DashboardWrapper>
+          <NextTopLoader showSpinner={false} />
+          {children}
+        </DashboardWrapper>
       </body>
     </html>
   );
