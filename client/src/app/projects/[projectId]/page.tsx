d1841e3e31695b7d811ade25b30be33eac437141
Prajwal-PL <prajwalpl096@gmail.com>
2024-09-20 21:05:29 +0530

    working on Board component

---
+++ client/src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React, { useState } from "react";
+import ProjectHeader from "../ProjectHeader";
+
+type Props = {
+  params: { id: string };
+};
+
+const ProjectPage = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+  return (
+    <div>
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+    </div>
+  );
+};
+
+export default ProjectPage;
